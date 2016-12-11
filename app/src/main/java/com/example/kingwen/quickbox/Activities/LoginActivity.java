2283cc82f495349d300e441de5c0bcba8e692b85
aweng126 <13075313106@163.com>
2016-12-11 23:52:39 +0800

    登录注册界面完成，等待优化

---
+++ app/src/main/java/com/example/kingwen/quickbox/Activities/LoginActivity.java
@@ -0,0 +1,105 @@
+package com.example.kingwen.quickbox.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.kingwen.quickbox.R;
+
+import butterknife.BindView;
+import butterknife.compiler.ButterKnifeProcessor;
+
+/**
+ * Created by kingwen on 2016/12/11.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * 输入用户名的edittext控件
+     */
+    private EditText et_name;
+
+    /**
+     * 输入用户密码的edittext控件
+     */
+    private EditText et_pass;
+
+    /**
+     * 确认登录的button控件
+     */
+    private Button btn_login;
+
+    /**
+     * 用于保存用户名控件内容的变量
+     */
+    private String name;
+
+    /**
+     * 用于保存密码控件内容的变量
+     */
+    private  String pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initViews();
+
+        initListener();
+
+    }
+
+    /**
+     * 初始化view
+     */
+    private void initViews() {
+        et_name= (EditText) findViewById(R.id.et_inputName_login);
+        et_pass= (EditText) findViewById(R.id.et_inputPassword_login);
+        btn_login= (Button) findViewById(R.id.btn_login);
+
+    }
+
+    /**
+     * 设置button的监听事件
+     */
+    private void initListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name=et_name.getText().toString();
+                pass=et_pass.getText().toString();
+                if(judgeLegal(name,pass)){
+
+                    Intent intent =new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+
+                }else {
+                    Toast.makeText(LoginActivity.this,"用户名或密码不合法,请重新输入",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    /**
+     * 判断用户名和密码是否符合合法
+     * @param name  需要判断的用户名
+     * @param pass  需要判断的用户名对应的密码
+     * @return true  合法 ； false 不合法
+     */
+    private boolean judgeLegal(String name, String pass) {
+
+        if("".equals(name)||"".equals(pass)){
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/kingwen/quickbox/Activities/RegisterActivity.java
@@ -0,0 +1,119 @@
+package com.example.kingwen.quickbox.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.kingwen.quickbox.R;
+
+/**
+ * Created by kingwen on 2016/12/11.
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+    /**
+     * 输入用户名的edittext控件
+     */
+    private EditText et_name;
+
+    /**
+     * 输入用户密码的edittext控件
+     */
+    private EditText et_pass;
+
+    /**
+     * 输入用户密码的edittext控件
+     */
+    private EditText et_confirmpass;
+
+    /**
+     * 确认登录的button控件
+     */
+    private Button btn_register;
+
+    /**
+     * 用于保存用户名控件内容的变量
+     */
+    private String name;
+
+    /**
+     * 用于保存密码控件内容的变量
+     */
+    private  String pass;
+
+
+    /**
+     * 用于保存确认密码控件内容的变量
+     */
+    private  String confirmpass;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        initViews();
+
+        initListeners();
+
+
+    }
+
+    /**
+     * 初始化view
+     */
+    private void initViews() {
+        et_name= (EditText) findViewById(R.id.et_inputName_register);
+        et_pass= (EditText) findViewById(R.id.et_inputPassword_register);
+        et_confirmpass= (EditText) findViewById(R.id.et_inputPassword_confirm);
+        btn_register= (Button) findViewById(R.id.btn_register);
+
+
+    }
+
+    /**
+     * 添加监听事件
+     */
+    private void initListeners() {
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name=et_name.getText().toString();
+                pass=et_pass.getText().toString();
+                confirmpass=et_confirmpass.getText().toString();
+
+                if(judgeLegal(name,pass,confirmpass)){
+
+                    Intent intent =new Intent(RegisterActivity.this,LoginActivity.class);
+                    startActivity(intent);
+
+                }else {
+                    Toast.makeText(RegisterActivity.this, "用户名或密码不合法,请重新输入", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    /**
+     * 判断用户名和密码是否符合合法
+     * @param name  需要判断的用户名
+     * @param pass  需要判断的用户名对应的密码
+     * @return true  合法 ； false 不合法
+     */
+    private boolean judgeLegal(String name, String pass,String confirmpass) {
+
+        if("".equals(name)||"".equals(pass)||!pass.equals(confirmpass)){
+            return false;
+        }
+
+        return true;
+    }
+}
